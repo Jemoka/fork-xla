ce73c2b45ebc6088fac7c9778ce18fdd5e4aef83
Houjun Liu <houjun@jemoka.com>
2025-12-23 07:38:10 -1000

    setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,10 +7,10 @@ echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] https://packages.clou
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.asc
 sudo apt-get update
 sudo apt-get install gcsfuse
-# mkdir data
+mkdir data
 mkdir checkpoints
-# gcsfuse --implicit-dirs --file-mode=777 --dir-mode=777 thoughtbubbles_data ./data
-gcsfuse --implicit-dirs --file-mode=777 --dir-mode=777 thoughtbubbles_checkpoints_uc2 ./checkpoints
+gcsfuse --implicit-dirs --file-mode=777 --dir-mode=777 thoughtbubbles_data ./data
+gcsfuse --implicit-dirs --file-mode=777 --dir-mode=777 thoughtbubbles_checkpoints_ue1 ./checkpoints
 
 # repo
 curl -LsSf https://astral.sh/uv/install.sh | sh
