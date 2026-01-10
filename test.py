f5e4a9bc8d27738c1e76be3867c2af1d47b3ece9
Houjun Liu <houjun@jemoka.com>
2026-01-10 15:07:03 -0800

    minor

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import jax
 
 jax.distributed.initialize()
 
-trainer = Pretrainer.from_pretrained("/home/houjun/checkpoints/final_pretrain_1_9b_regular/best")
+trainer = Pretrainer.from_pretrained("/home/houjun/checkpoints/final_pretrain_1_9b_regular/checkpoint/61440")
 
 print(jax.device_get(trainer.state).opt_state[1][0].mu["blocks_0"]["attn"]["c_proj"]["kernel"].value.mean())
 print(jax.device_get(trainer.state).opt_state[1][0].mu["blocks_0"]["attn"]["c_proj"]["kernel"].value)
