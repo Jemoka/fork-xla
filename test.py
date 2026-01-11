998d1b83fb398bc9b4e762721284d76d050b2de4
Houjun Liu <houjun@jemoka.com>
2026-01-10 16:11:09 -0800

    valid_step()

--- test.py
+++ test.py
@@ -5,4 +5,8 @@ jax.distributed.initialize()
 
 trainer = Pretrainer.from_pretrained("/home/houjun/checkpoints/final_pretrain_1_9b_regular/checkpoint/61440")
 
-print(jax.tree_util.tree_reduce(lambda carry, xs:carry+xs, jax.tree_util.tree_map(lambda x:x.mean(), trainer.state.params)))
+vs = trainer.make_valid_step()
+print(vs(trainer.state))
+
+
+# print(jax.tree_util.tree_reduce(lambda carry, xs:carry+xs, jax.tree_util.tree_map(lambda x:x.mean(), trainer.state.params)))
